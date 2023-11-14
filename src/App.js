6342fcacadce69997a16233379d99ab3fcea5303
Dmitriy <dimabar208@gmail.com>
2023-11-14 15:22:59 +0300

    обновил шапку

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import './styles/build/App.css';
+import './styles/App.css';
 import Zebra from './svg/zebra';
+import swimming from './pic/swimming.png'
+import cycling from './pic/cycling.png'
+import running from './pic/running.png'
 
 function App() {
   return (
     <div className="App">
       <div className='generall_page_wrapper'>
-        <div className='main>'>
+        <div className='main'>
           <div className='main_title'>
             <h1>Baruzdin.tri</h1>
           </div>
-          <div className='main_menu'>
-            <h3>About</h3>
-            <h3>Coach</h3>
-            <h3>Program</h3>
-            <h3>FAQ</h3>
-            <h3>Payment</h3>
-            <h3>Utility</h3>
-          </div>
-          <div className='main_pic'>
-            <div>pic1</div>
-            <Zebra/>
-            <div>pic2</div>
-            <div>pic3</div>
-          </div>
-          <div className='main_slogan'>
-            <h2>План твоих достижений.</h2>
+          <ul className='main_menu'>
+            <li>About</li>
+            <li>Coach</li>
+            <li>Program</li>
+            <li>FAQ</li>
+            <li>Payment</li>
+            <li>Utility</li>
+          </ul>
+          <div className='main_page'>
+            <div className='main_page_pic'>
+              <img src={swimming} alt='swimming'/>
+              <img src={cycling} className='style__shadow' alt='cycling'/>
+              <img src={running} alt='running'/>
+            </div>
+            <div className='main_page_slogan'>
+              <h2>План твоих достижений.</h2>
+            </div>
           </div>
           <div>
             <button className='main_button'>Начать тренировки</button>
           </div>
-
         </div>
       </div>
     </div>
