eff295ed727674265cf7941342f847a51d20e15c
Dmitriy Baruzdin <dimabar208@gmail.com>
2023-11-24 14:57:18 +0300

    тут сверстал второй блок для декстопа, без адаптива

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Zebra from './svg/zebra';
+import Zebra from './svg/zebra'
 import '@csstools/normalize.css'
 import './styles/App.css'
 import swimming from './pic/swimming.png'
 import cycling from './pic/cycling.png'
 import running from './pic/running.png'
+import phoneTP from './pic/phoneTP.png'
+import macBook from './pic/macBook.png'
+import phone from './pic/phone.png'
+import gym from './pic/gym.png'
 import './fonts/Oswald/Oswald-Light.ttf'
 import './fonts/VT323/VT323-Regular.ttf'
 
@@ -41,11 +45,20 @@ function App() {
           <button className='main_button'>Начать тренировки</button>
         </div>
         <div className='about'>
-          <div className='question_one'>
+          <div className='about_asside'>
+            <div>About</div>
+            <span></span>
+          </div>
+          <div className='about_question_one'>
             <h3>Что такое online план тренировок?</h3>
             <p>Это календарь с&nbsp;тренировками на&nbsp;каждый день. Календарь составляется в&nbsp;приложении Training Pekas. Приложение позволяет подробно анализировать результаты тренировок и&nbsp;соревнований, а&nbsp;так&nbsp;же интегрируется с&nbsp;популярными приложениями и&nbsp;устройствами.</p>
           </div>
-          <div className='question_two'>
+          <div className='about_phones'>
+            <img src={phone} alt="phone" />
+            <img src={phoneTP} alt="phoneTP" />
+          </div>
+          <img className='about_macBook' src={macBook} alt="macBook"/>
+          <div className='about_question_two'>
             <h3>Бег, плавание, вело, а&nbsp;может триатлон?</h3>
             <ul>
               <li>Разнообразие тренировок и&nbsp;фокус на&nbsp;результате</li>
@@ -53,9 +66,9 @@ function App() {
               <li>Связь с&nbsp;тренером и&nbsp;корректировки online</li>
               <li>Подготовка к&nbsp;различным дисциплинам и&nbsp;дистанциям</li>
             </ul>
-            <Zebra/>
           </div>
-          <div className='question_three'>
+          <Zebra/>
+          <div className='about_question_three'>
             <h3>Ваш календарь уникален и&nbsp;учитывает:</h3>
             <ul>
               <li>Целевой старт</li>
@@ -65,7 +78,7 @@ function App() {
               <li>Командировки, болезни</li>
             </ul>
           </div>
-          <div className='question_four'>
+          <div className='about_question_four'>
             <h3>Что необходимо для занятий?</h3>
             <ul>
               <li>Спортивные часы с&nbsp;пульсометром</li>
@@ -73,7 +86,7 @@ function App() {
               <li>Велосипед, велосипедный станок</li>
             </ul>
           </div>
-          <div className='question_five'>
+          <div className='about_question_five'>
             <h3>Какой минимальный начальный уровень?</h3>
             <ul>
               <li>Плавание&nbsp;&mdash; не&nbsp;более 23&nbsp;мин/1км</li>
@@ -81,6 +94,11 @@ function App() {
                 + езда в&nbsp;контактных педалях.</li>
               <li>Бег&nbsp;&mdash; не&nbsp;более 35&nbsp;мин/5км</li>
             </ul>
+          </div>
+          <div className='about_gym'>
+            <div className='about_gym_background'>
+              <h4 className='about_gym_text'>План включает в&nbsp;себя не&nbsp;только тренировки по&nbsp;целевым видам, но&nbsp;и&nbsp;занятия в&nbsp;зале (ОФП, СФП), а&nbsp;так&nbsp;же упражнния направленные на&nbsp;улучшение координации, баланса, подвижности.</h4>
+            </div>
           </div>
         </div>
       </div>
