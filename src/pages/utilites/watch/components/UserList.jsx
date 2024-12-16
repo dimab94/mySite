782c89017df8566a1193bb9886dab87d8661f2c7
Dmitriy Baruzdin <dimabar208@gmail.com>
2024-12-16 18:34:51 +0300

    сделал нормальную навигацию по группам

--- src/pages/utilites/watch/components/UserList.jsx
+++ src/pages/utilites/watch/components/UserList.jsx
@@ -18,10 +18,10 @@ function UserList({props, userArr,reset}) {
       users.reduce((acc, user) => {
         // Если группы еще нет в аккумуляторе, добавляем её как пустой массив
         if (!acc[user.group]) {
-          acc[user.group] = [];
+          acc[user.group] = {id:user.group,arr:[]};
         }
         // Добавляем пользователя в соответствующую группу
-        acc[user.group].push(user);
+        acc[user.group].arr.push(user);
         return acc;
       }, {})
     );
@@ -43,51 +43,23 @@ function UserList({props, userArr,reset}) {
   const groupedUsers = groupUsersByGroup(users);
 
   //drag & drop below-----------------------------------------------
-
-/*   function dragStartHandler(e,user){
-    setCurrentUser(user)
-  }
-  function dragEndHandler(e){
-  }
-  function dragOverHandler(e){
-      e.preventDefault()
-  }
-  function dropHandler(e,user){
-      e.preventDefault()
-      console.log(user)
-      console.log("Состояние currentUser обновлено:", currentUser)
-        const updatedUsers = mainArrUsers.map((u)=>{
-          if (u.id == draggedUser.id){
-              u.group = user.group
-              console.log(updatedUsers)
-          }
-      })
-  } */
     
   return (
     <div className="users">
       {groupedUsers.map((group, index) => (
           <div className="group_list" key={index} style={{backgroundColor: colors[index % colors.length],border:colors[index % colors.length]}}>
               <div className="group_wrapper">
-                  <div className="group_name" onClick={()=>HideAndBlock(group[0].group)}>
-                      <p className="group_item">{(group[0].group!=-1)?group[0].group:'--'} Группа</p>
-                      {(group.length>1)
+                  <div className="group_name" onClick={()=>HideAndBlock(group.id)}>
+                      <p className="group_item">{(group.id!==-1)?group.id:'--'} Группа</p>
+                      {(group.arr.length>1)
                         ?<ArrowDropDownIcon/>
                         :<div/>
                       }
                   </div>
-                  <button className="group_button" onClick={()=>setGropupLap(group[0].group)}><RestartAltIcon/></button>
+                  <button className="group_button" onClick={()=>setGropupLap(group.id)}><RestartAltIcon/></button>
               </div>
-              {group.map((user, index) => (
+              {group.arr.map((user, index) => (
                   <User props={props} 
-
-                  
-/*                   onDragStart={(e)=>dragStartHandler(e,user)}
-                  onDragLeave={(e)=>dragEndHandler(e)}
-                  onDragEnd={(e)=>dragEndHandler(e)}
-                  onDragOver={(e)=> dragOverHandler(e)}
-                  onDrop={(e)=>dropHandler(e,user)} */
-
                   user={user} 
                   reset={reset} 
                   key={index} 
@@ -103,3 +75,5 @@ function UserList({props, userArr,reset}) {
 }
 
 export default UserList;
+
+//баг при добавлении нового пользователя - сбрысывается время и круги.
